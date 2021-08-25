f8134c5b479e7cbb3759ad52f32c7c4b96a02ff6
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-08-25 04:10:27 +0300

    Add files via upload

---
+++ arch.py
@@ -0,0 +1,230 @@
+import os 
+from colorama import Fore, Back, Style, init
+init(autoreset=True)
+print(Fore.LIGHTGREEN_EX + """ 
+ ██░ ██  ▄▄▄       █     █░██ ▄█▀    ▒█████    █████▒▄████▄  ▒██   ██▒
+▓██░ ██▒▒████▄    ▓█░ █ ░█░██▄█▒    ▒██▒  ██▒▓██   ▒▒██▀ ▀█  ▒▒ █ █ ▒░
+▒██▀▀██░▒██  ▀█▄  ▒█░ █ ░█▓███▄░    ▒██░  ██▒▒████ ░▒▓█    ▄ ░░  █   ░
+░▓█ ░██ ░██▄▄▄▄██ ░█░ █ ░█▓██ █▄    ▒██   ██░░▓█▒  ░▒▓▓▄ ▄██▒ ░ █ █ ▒ 
+░▓█▒░██▓ ▓█   ▓██▒░░██▒██▓▒██▒ █▄   ░ ████▓▒░░▒█░   ▒ ▓███▀ ░▒██▒ ▒██▒
+ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▓░▒ ▒ ▒ ▒▒ ▓▒   ░ ▒░▒░▒░  ▒ ░   ░ ░▒ ▒  ░▒▒ ░ ░▓ ░
+ ▒ ░▒░ ░  ▒   ▒▒ ░  ▒ ░ ░ ░ ░▒ ▒░     ░ ▒ ▒░  ░       ░  ▒   ░░   ░▒ ░
+ ░  ░░ ░  ░   ▒     ░   ░ ░ ░░ ░    ░ ░ ░ ▒   ░ ░   ░         ░    ░  
+ ░  ░  ░      ░  ░    ░   ░  ░          ░ ░         ░ ░       ░    ░  
+                                                    ░                 
+""")
+print(Fore.LIGHTBLUE_EX + """ 
+
+1 -> Wireless networks and hacking Tools
+
+2 -> Information Gathering Tools 
+
+3 -> Password Cracker Tools
+
+4 -> System , Terminal update 
+
+ """)
+tool = input(" -> ")
+if tool=="1":
+    os.system("cd")
+    os.system("sudo apt-get install -y aircrack-ng")
+    os.system("git clone https://github.com/derv82/wifite2")
+    os.system("https://github.com/wifiphisher/wifiphisher")
+    os.system("sudo apt-get install wireshark")
+    os.system("https://github.com/v1s1t0r1sh3r3/airgeddon")
+    os.system("git clone https://github.com/hawk-unity/asistan")
+    os.system("git clone https://github.com/hawk-unity/xssvuln")
+    os.system("git clone https://github.com/hawk-unity/adminpanel")
+    os.system("git clone https://github.com/shawarkhanethicalhacker/D-TECT-1")
+    os.system("git clone https://github.com/hawk-unity/iptracer")
+    os.system("git clone https://github.com/hawk-unity/ddosab")
+    os.system("git clone https://github.com/s0md3v/ReconDog")
+    os.system("sudo apt-get install nikto")
+    os.system("git clone https://github.com/CiKu370/ko-dork")
+    os.system("git clone https://github.com/br0keh/cc-checker")
+    os.system("git clone https://github.com/dotfighter/torshammer")
+    os.system("git clone https://github.com/jseidl/GoldenEye")
+    os.system("git clone https://github.com/cyweb/hammer")
+    os.system("git clone https://github.com/sepehrdaddev/Xerxes")
+    os.system("git clone https://github.com/HatBashBR/HatCloud")
+    os.system("git clone https://github.com/s0md3v/Hash-Buster")
+    os.system("git clone https://github.com/gkbrk/slowloris")
+    os.system("git clone https://github.com/sankamuk/HostChecker")
+    os.system("sudo apt-get install weevely")
+    os.system("sudo apt-get install jsql")
+    print("                               ")
+    print("                               ")
+    print("                               ")
+    print(" AİRCRACK-NG İNSTALLED ")
+    print("                               ")
+    print("                               ")
+    print("                               ")
+    print("WİFİTE2 İNSTALLED")
+    print("                               ")
+    print("                               ")
+    print("                               ")
+    print(" WİFİ PHİSHER İNSTALLED ")
+    print("                               ")
+    print("                               ")
+    print("                               ")
+    print(" WİRESHARK İNSTALLED ")
+    print("                               ")
+    print("                               ")
+    print("                               ")
+    print(" AİRGEDDON İNSTALLED ")
+    print("                               ")
+    print("                               ")
+    print("                               ")
+    print(" OTHER TOOLS İNSTALLED ...........................")
+if tool=="2":
+    os.system("cd")
+    os.system("sudo apt-get install sqlmap")
+    os.system("sudo apt-get install urlcrazy")
+    os.system("git clone https://github.com/Moham3dRiahi/Th3inspector")
+    os.system("git clone https://github.com/cys3c/RED_HAWK")
+    os.system("git clone https://github.com/evyatarmeged/Raccoon")
+    os.system("git clone --recursive https://github.com/trimstray/sandmap")
+    os.system("sudo apt-get install dmitry")
+    os.system("sudo apt-get install dnsmap")
+    os.system("git clone https://github.com/hawk-unity/hackingtools")
+    os.system("git clone https://github.com/hawk-unity/toolcuk")
+    os.system("git clone https://github.com/hawk-unity/port")
+    os.system("git clone https://github.com/hawk-unity/hawq")
+    os.system(" git clone https://github.com/s0md3v/ReconDog")
+    print(""" 
+    
+    
+    
+
+    """)
+    print(" SQLMAP İNSTALLED ")
+    print(""" 
+    
+    
+    
+
+    """)
+    print(" URLCRAZY İNSTALLED ")
+    print(""" 
+    
+    
+    
+    
+    """)
+    print("TH3İNSPECTOR İNSTALLED ")
+    print(""" 
+    
+    
+    
+    
+    """)    
+    print(" RED HAWK İNSTALLED ")
+    print(""" 
+    
+    
+    
+    
+    """)    
+    print(" RACCOON İNSTALLED ")
+    print(""" 
+    
+    
+    
+    
+    """)    
+    print(" DMİTRY İNSTALLED ")
+    print(""" 
+    
+    
+    
+    
+    """)    
+    print(" DNSMAP İNSTALLED ")
+    print(""" 
+    
+    
+    
+    
+    """)    
+    print(" HACKİNGTOOLS İNSTALLED ")
+    print(""" 
+    
+    
+    
+    
+    """)    
+    print(" TOOLCUK İNSTALLED ")       
+    print(""" 
+    
+    
+    
+    
+    """) 
+    print(" PORT SCANNER İNSTALLED ")
+    print(""" 
+    
+    
+    
+    
+    """)
+    print(" HAWQ İNSTALLED ")
+    print(""" 
+    
+    
+    
+    
+    """)
+if tool=="3":
+    os.system("cd")
+    os.system("sudo apt-get install hashcat")
+    os.system("sudo apt-get install hydra")
+    os.system("sudo apt-get install john -y")
+    os.system("sudo apt-get install medusa")
+    os.system("git clone https://github.com/hawk-unity/hawmd5")
+    print(""" 
+    
+    
+    
+    
+    """)
+    print(" HASHCAT İNSTALLED ")
+    print(""" 
+    
+    
+    
+    
+    """)
+    print(" HYDRA İNSTALLED ")
+    print(""" 
+    
+    
+    
+    
+    """)
+    print(" JOHN İNSTALLED ")
+    print(""" 
+    
+    
+    
+    
+    """)
+    print(" MEDUSA İNSTALLED ")
+    print(""" 
+    
+    
+    
+    
+    """)
+    print(" MD5 CRACKER İNSTALLED ")
+
+if tool=="4":
+   os.system("cd")
+   os.system("sudo apt install toilet -y")
+   os.system("sudo apt install libcurl -y")
+   os.system("sudo apt install perl -y")
+   os.system("sudo apt install figlet -y")
+   os.system("sudo apt install ruby -y")
+   os.system("sudo apt install wget -y")
+   os.system("sudo apt install python -y")
+   os.system("sudo apt-get install git")
+   os.system("sudo apt install nano -y")
